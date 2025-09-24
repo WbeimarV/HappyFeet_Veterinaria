d41ea96c97b8ed7b72afee3828413337edb58437
Wbeimar Vega <94907591+WbeimarV@users.noreply.github.com>
2025-09-24 15:52:21 -0500

    Creacion del proyecto

---
+++ src/main/java/com/happyfeet/happyfeet_veterinaria/HappyFeet_Veterinaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.happyfeet.happyfeet_veterinaria;
+
+/**
+ *
+ * @author camper
+ */
+public class HappyFeet_Veterinaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
